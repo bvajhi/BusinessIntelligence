73d3bd007ecef4feb7ad6b4cee5c2c184092f11c
bvajhi <bvajhi@gmail.com>
2022-02-12 10:42:13 -0600

    got the name of the companies displaying

--- screens/Businesses.js
+++ screens/Businesses.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { FlatList, Text } from 'react-native';
 
+import businessJSON from '../data.json';
+
 export default class Businesses extends React.Component {
   render() {
-    return <Text>Foo bar shibby</Text>
-    // return <FlatList />
+    return <FlatList
+      data={businessJSON}
+      renderItem={(element) => {
+      console.log(element)
+      return <Text>{element.item.name}</Text>
+    }}
+      keyExtractor={(item) => {
+        return item.name + item.id;
+      }}
+      showsVerticalScrollIndicator={false}
+    />
   }
 }
